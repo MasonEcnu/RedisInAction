3d25cbba704f059379f4ba475e1a1eb9c4ec7519
MasonEcnu <364207187@qq.com>
2019-08-24 17:35:14 +0800

    Chapter 4 and part of Chapter 5 to page 115

--- com/mason/redis/part_two/chapter05/chapter052.py
+++ com/mason/redis/part_two/chapter05/chapter052.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import bisect
 import time
 
+import redis
 from redis import Redis
 
 # 计数器和数据统计
@@ -40,3 +42,59 @@ def get_counter(conn: Redis, name, precision):
     # 按key升序，key相同时，按value升序
     to_return.sort()
     return to_return
+
+
+# 在处理（process）和清理（clean up）旧计数器的时候，有几件事情是需要我们格外留心的，
+# 其中包括以下几件。
+# 1.任何时候都可能会有新的计数器被添加进来。
+# 2.同一时间可能会有多个不同的清理操作在执行。
+# 3.对于一个每天只更新一次的计数器来说，以每分钟一次的频率尝试清理这个计数器只会
+# 浪费计算资源。
+# 4.如果一个计数器不包含任何数据，那么程序就不应该尝试对它进行清理。
+
+
+QUIT_FLAG = False
+SAMPLE_COUNT = 100
+
+
+def clean_counters(conn: Redis):
+    pipe = conn.pipeline(True)
+    passes = 0
+    while not QUIT_FLAG:
+        start = time.time()
+        index = 0
+        while index < conn.zcard("known:"):
+            hash_key = conn.zrange("known:", index, index)
+            index += 1
+            if not hash_key:
+                break
+            hash_key = hash_key[0]
+            prec = int(hash_key.partition(":")[0])
+            bprec = int(prec // 60) or 1
+            if passes % bprec:
+                continue
+
+            hkey = "count:" + hash_key
+            cutoff = time.time() - SAMPLE_COUNT * prec
+            samples = list(map(int, conn.hkeys(hkey)))
+            samples.sort()
+
+            remove = bisect.bisect_right(samples, cutoff)
+            if remove:
+                conn.hdel(hkey, *samples[:remove])
+                if remove == len(samples):
+                    try:
+                        pipe.watch(hkey)
+                        if not pipe.hlen(hkey):
+                            pipe.multi()
+                            pipe.zrem("known:", hash_key)
+                            pipe.execute()
+                            index -= 1
+                        else:
+                            pipe.unwatch()
+                    except redis.exceptions.WatchError:
+                        pass
+
+        passes += 1
+        duration = min(int(time.time() - start) + 1, 60)
+        time.sleep(max(60 - duration, 1))
