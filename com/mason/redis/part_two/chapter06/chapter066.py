e5ccac4fe14189a158d827258a8f1dd213077d71
MasonEcnu <364207187@qq.com>
2019-09-11 20:59:33 +0800

    Chapter 5 and part of Chapter 6 to page 168

--- com/mason/redis/part_two/chapter06/chapter066.py
+++ com/mason/redis/part_two/chapter06/chapter066.py
@@ -1,4 +1,45 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import csv
+from collections import defaultdict
 
 # 文件分发
+# 一个本地聚合计算回调函数
+# 用于每天以国家维度对日志行进行聚合
+from redis import Redis
+
+aggregates = defaultdict(lambda: defaultdict(int))
+
+
+def daily_country_aggregate(conn: Redis, line: str):
+    if line:
+        # 默认按空格分割
+        line = line.split()
+        ip = line[0]
+        day = line[1]
+        country = find_city_by_ip_local(ip)[2]
+        if not country:
+            country = -1
+        aggregates[day][country] += 1
+        return
+
+    for day, aggregate in aggregates.items():
+        conn.zadd("daily:country:" + day, **aggregate)
+        del aggregates[day]
+
+
+def find_city_by_ip_local(ip: str):
+    ip_file = r"D:\PyCode\RedisInAction\location_file\GeoLite2-City-CSV_20190820\GeoLite2-City-Blocks-IPv4.csv"
+    csv_file = csv.reader(open(ip_file, "r"))
+    for count, row in enumerate(csv_file):
+        # 将Ip地址转为分值
+        start_ip = row[0] if row else ""
+        # 略过文件的列名行和格式错误行
+        if "i" in start_ip.lower() or start_ip == "":
+            continue
+        if "/" in start_ip:
+            start_ip = start_ip.split("/")[0]
+        if "." in start_ip and start_ip.__eq__(ip):
+            return row
+        else:
+            return None
